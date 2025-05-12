2bf873de73c83c9ca9e8a53f525d261e804ca4f0
Tee <chakid.c@kisracorp.com>
2025-05-12 16:17:01 +0700

    update entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# พยายาม migrate จนกว่าจะสำเร็จ
 until php artisan migrate --force; do
   echo "⏳ Waiting for MySQL to be ready..."
-  sleep 10
+  sleep 5
 done
 
-php artisan db:seed
+echo "✅ Migrations done."
 
+# รัน db:seed เมื่อ migrate สำเร็จ
+php artisan db:seed --force
+echo "✅ Seeding done."
+
+# เริ่ม Laravel server
 php artisan serve --host=0.0.0.0 --port=8000
